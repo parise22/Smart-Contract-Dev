aa57151abdf6f2a17a7dde604548ebe1091e83c7
Danny <43259125+parise22@users.noreply.github.com>
2022-02-20 17:35:41 +1100

    Adding Features/Owner

--- Contracts/ZombieFeeding.sol
+++ Contracts/ZombieFeeding.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.4.25;
 
 import "./ZombieFactory.sol";
+
 abstract contract KittyInterface {
   function getKitty(uint256 _id) external view virtual returns (
     bool isGestating,
@@ -16,10 +17,14 @@ abstract contract KittyInterface {
     uint256 genes
   );
 }
+
 contract ZombieFeeding is ZombieFactory {
 
-  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-  KittyInterface kittyContract = KittyInterface(ckAddress);
+  KittyInterface kittyContract;
+
+  function setKittyContractAddress(address _address) external onlyOwner() {
+    kittyContract = KittyInterface(_address);
+  }
 
   function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
